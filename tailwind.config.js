b0aa9609f501b25d4867b237a65f3b3252051f7d
Ormon-huehuehue <armnvrma10@gmail.com>
2025-01-08 23:34:56 +0530

    kaafi kuch kardiya

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./tradeTheme/**/*.liquid"],
+  theme: {
+    extend: {
+      colors:{
+        limeCircles : "#cdf986",
+        oliveGreen : "#182018"
+      }
+    },
+  },
+  plugins: [],
+}
+
